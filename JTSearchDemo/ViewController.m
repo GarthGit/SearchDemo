ab39095ce496dd960684e6e16e5bb2d7128ed5b3
hoyifo <hoyifo@hoyifodemac-mini.local>
2018-12-05 15:10:03 +0800

    Initial Commit

---
+++ JTSearchDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JTSearchDemo
+//
+//  Created by hoyifo on 2018/12/5.
+//  Copyright © 2018 hoyifo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ JTSearchDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JTSearchDemo
+//
+//  Created by hoyifo on 2018/12/5.
+//  Copyright © 2018 hoyifo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
